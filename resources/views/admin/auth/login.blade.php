669904cf1d03bc5b9d86669df2404ced7d7cd544
Chandima Samarakoon <chandima.s@eyepax.com>
2023-03-07 22:22:57 +0000

    Created the logo, welcome page changes

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,9 +1,7 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+        <img class="object-contain h-20 w-80 ..." src="{{ asset('images/logo.png') }}">
             Admin Login
         </x-slot>
 
